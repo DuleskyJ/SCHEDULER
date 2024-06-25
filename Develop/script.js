378b92be294926f94850ce012e410da6832844fb
Laptop <jdkyaz@gmail.com>
2024-06-24 18:45:27 -0700

    Removed self made timeblocks to create the foundation to have them dynamically generated with JQuery.

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,36 @@
 $(function () {
-  // Display the current date in the header
-  $('#currentDay').text(dayjs().format('dddd, MMMM D'));
+  // Displaying the current date in header
+  const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(currentDate);
 
-  // listener for click events on save button
-  $('.saveBtn').on('click', function () {
-    var timeBlockId = $(this).parent().attr('id');
-    var value = $(this).siblings('.description').val();
-    localStorage.setItem(timeBlockId, value);
+  // Generating time blocks for each hour 9-5
+  const container = $('.container-fluid');
+  const workHours = [
+    '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'
+  ];
+
+  workHours.forEach((hour, index) => {
+    const hour24 = index + 9; // Convert index to 24-hour format
+
+    const timeBlock = $(`
+      <div id="hour-${hour24}" class="row time-block">
+        <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
+        <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
+    `);
+
+    container.append(timeBlock);
   });
 
-  // Apply the past, present, or future class to each time block
+  // Apply past, present, or future class to each time block
   function updateTimeBlocks() {
-    var currentHour = dayjs().hour();
+    const currentHour = dayjs().hour();
+
     $('.time-block').each(function () {
-      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+      const blockHour = parseInt($(this).attr('id').split('-')[1]);
 
       if (blockHour < currentHour) {
         $(this).addClass('past').removeClass('present future');
@@ -25,14 +42,25 @@ $(function () {
     });
   }
 
-  // Get any user input that was saved in localStorage and set the values of the corresponding textarea elements
-  $('.time-block').each(function () {
-    var timeBlockId = $(this).attr('id');
-    var value = localStorage.getItem(timeBlockId);
-    if (value) {
-      $(this).find('.description').val(value);
-    }
+  updateTimeBlocks();
+  setInterval(updateTimeBlocks, 60000); // Updating every minute
+
+  // Save events to local storage
+  $('.saveBtn').on('click', function () {
+    const timeBlock = $(this).closest('.time-block');
+    const hour = timeBlock.attr('id');
+    const description = timeBlock.find('.description').val();
+
+    localStorage.setItem(hour, description);
   });
 
-  updateTimeBlocks();
+  // Load events from local storage
+  $('.time-block').each(function () {
+    const hour = $(this).attr('id');
+    const description = localStorage.getItem(hour);
+
+    if (description) {
+      $(this).find('.description').val(description);
+    }
+  });
 });
